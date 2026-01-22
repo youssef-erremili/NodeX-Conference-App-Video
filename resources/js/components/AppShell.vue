8b011d5143152e9e122bfe9e005d2035041ad731
youssef erremili <yousseferremili19@gmail.com>
2026-01-22 17:50:33 +0100

    fix: fix project setup

---
+++ resources/js/components/AppShell.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { SidebarProvider } from '@/components/ui/sidebar';
+import type { AppShellVariant } from '@/types';
+import { usePage } from '@inertiajs/vue3';
+
+type Props = {
+    variant?: AppShellVariant;
+};
+
+defineProps<Props>();
+
+const isOpen = usePage().props.sidebarOpen;
+</script>
+
+<template>
+    <div v-if="variant === 'header'" class="flex min-h-screen w-full flex-col">
+        <slot />
+    </div>
+    <SidebarProvider v-else :default-open="isOpen">
+        <slot />
+    </SidebarProvider>
+</template>
