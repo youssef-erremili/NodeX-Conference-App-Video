7063eb361598b49744ea9ede102f58a7bb8e7b33
YoussefErremili <yousseferremili.dev@gmail.com>
2026-01-23 20:13:47 +0100

    feature: create a home layout

--- resources/js/layouts/settings/Layout.vue
+++ resources/js/layouts/settings/Layout.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import { Link } from '@inertiajs/vue3';
 import Heading from '@/components/Heading.vue';
 import { Button } from '@/components/ui/button';
 import { Separator } from '@/components/ui/separator';
@@ -9,7 +10,6 @@ import { edit as editProfile } from '@/routes/profile';
 import { show } from '@/routes/two-factor';
 import { edit as editPassword } from '@/routes/user-password';
 import { type NavItem } from '@/types';
-import { Link } from '@inertiajs/vue3';
 
 const sidebarNavItems: NavItem[] = [
     {
@@ -35,27 +35,15 @@ const { isCurrentUrl } = useCurrentUrl();
 
 <template>
     <div class="px-4 py-6">
-        <Heading
-            title="Settings"
-            description="Manage your profile and account settings"
-        />
+        <Heading title="Settings" description="Manage your profile and account settings" />
 
         <div class="flex flex-col lg:flex-row lg:space-x-12">
             <aside class="w-full max-w-xl lg:w-48">
-                <nav
-                    class="flex flex-col space-y-1 space-x-0"
-                    aria-label="Settings"
-                >
-                    <Button
-                        v-for="item in sidebarNavItems"
-                        :key="toUrl(item.href)"
-                        variant="ghost"
-                        :class="[
-                            'w-full justify-start',
-                            { 'bg-muted': isCurrentUrl(item.href) },
-                        ]"
-                        as-child
-                    >
+                <nav class="flex flex-col space-y-1 space-x-0" aria-label="Settings">
+                    <Button v-for="item in sidebarNavItems" :key="toUrl(item.href)" variant="ghost" :class="[
+                        'w-full justify-start',
+                        { 'bg-muted': isCurrentUrl(item.href) },
+                    ]" as-child>
                         <Link :href="item.href">
                             <component :is="item.icon" class="h-4 w-4" />
                             {{ item.title }}
