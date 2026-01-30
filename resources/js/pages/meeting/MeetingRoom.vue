018f96a4bd2a26b51cbec9b7c0cbb2a3bd0d5b6b
YoussefErremili <yousseferremili.dev@gmail.com>
2026-01-30 23:59:25 +0100

    feature: made meeting room layout

--- resources/js/pages/meeting/MeetingRoom.vue
+++ resources/js/pages/meeting/MeetingRoom.vue
@@ -1,7 +1,16 @@
 <script lang="ts" setup>
-
 </script>
-
 <template>
-    <h1>meeting room</h1>
+    <div class="flex h-screen w-full gap-4 p-4">
+        <div class="flex flex-col flex-1 gap-4">
+            <div class="bg-gray-900 rounded-lg flex-1"></div>
+            <div class="flex gap-4 h-32">
+                <div class="bg-gray-800 rounded-lg flex-1"></div>
+                <div class="bg-gray-800 rounded-lg flex-1"></div>
+                <div class="bg-gray-800 rounded-lg flex-1"></div>
+            </div>
+        </div>
+
+        <div class="bg-gray-900 rounded-lg w-80"></div>
+    </div>
 </template>
