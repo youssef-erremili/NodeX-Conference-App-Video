a997c9af480fbb00b33862bb7ce8061208d726ac
YoussefErremili <yousseferremili.dev@gmail.com>
2026-01-30 01:01:29 +0100

    refactor: Redesign auth pages (login and sign up) to meet application perspective

--- resources/js/layouts/auth/AuthSimpleLayout.vue
+++ resources/js/layouts/auth/AuthSimpleLayout.vue
@@ -1,7 +1,5 @@
-<script setup lang="ts">
-import AppLogoIcon from '@/components/AppLogoIcon.vue';
-import { home } from '@/routes';
-import { Link } from '@inertiajs/vue3';
+<script lang="ts" setup>
+import AppLogo from '@/components/AppLogo.vue';
 
 defineProps<{
     title?: string;
@@ -10,33 +8,46 @@ defineProps<{
 </script>
 
 <template>
-    <div
-        class="flex min-h-svh flex-col items-center justify-center gap-6 bg-background p-6 md:p-10"
-    >
-        <div class="w-full max-w-sm">
-            <div class="flex flex-col gap-8">
-                <div class="flex flex-col items-center gap-4">
-                    <Link
-                        :href="home()"
-                        class="flex flex-col items-center gap-2 font-medium"
-                    >
-                        <div
-                            class="mb-1 flex h-9 w-9 items-center justify-center rounded-md"
-                        >
-                            <AppLogoIcon
-                                class="size-9 fill-current text-[var(--foreground)] dark:text-white"
-                            />
-                        </div>
-                        <span class="sr-only">{{ title }}</span>
-                    </Link>
-                    <div class="space-y-2 text-center">
-                        <h1 class="text-xl font-medium">{{ title }}</h1>
-                        <p class="text-center text-sm text-muted-foreground">
-                            {{ description }}
+    <div class="flex justify-evenly min-h-screen bg-[#fff]">
+        <!-- Left Side - Form -->
+        <div class="flex w-full flex-col lg:w-[45%]">
+            <div class="flex min-h-screen flex-col">
+                <!-- Logo -->
+                <div class="px-8 py-6 lg:px-12">
+                    <AppLogo />
+                </div>
+
+                <!-- Form Content -->
+                <div class="flex flex-1 items-center justify-center px-8 py-12 lg:px-16">
+                    <div class="w-full max-w-md">
+                        <slot />
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- Right Side -->
+        <div class="flex flex-col w-full lg:w-[45%]">
+            <div class="relative h-screen py-2 w-full lg:px-16">
+                <img
+                    alt="Modern Architecture"
+                    class="w-full h-full rounded-md object-cover"
+                    src="/images/woman-wallpaper.webp"
+                />
+
+                <div class="absolute inset-0 bg-black/10 rounded-md pointer-events-none lg:mx-16 my-2"></div>
+
+                <div
+                    class="absolute left-[5rem] bottom-10 z-20 w-[25rem] bg-black/40 backdrop-blur-xl border border-white/10 shadow-2xl flex items-center justify-between p-6 rounded-2xl">
+                    <div>
+                        <p class="text-lg font-semibold text-white tracking-wide">Youssef Erremili</p>
+                        <p class="text-sm text-white/80 font-medium">Founder</p>
+                        <p class="text-xs text-white/50 mt-1 uppercase tracking-tighter">Global Real
+                            <Estate></Estate>
+                            Investment Firm
                         </p>
                     </div>
                 </div>
-                <slot />
             </div>
         </div>
     </div>
