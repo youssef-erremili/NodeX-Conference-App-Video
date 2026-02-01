99d5b5927cb081a448b2d1fe386a944acd522ede
YoussefErremili <yousseferremili.dev@gmail.com>
2026-02-01 02:37:24 +0100

    fix: fix conflit

--- resources/js/pages/meeting/MeetingRoom.vue
+++ resources/js/pages/meeting/MeetingRoom.vue
@@ -1,4 +1,5 @@
 <script lang="ts" setup>
+import { ref } from 'vue';
 import GrayLayer from '@/components/GrayLayer.vue';
 import Microphone from '@/components/Microphone.vue';
 import Button from '@/components/ui/button/Button.vue';
@@ -134,6 +135,50 @@ const participants = ref([
             </div>
         </div>
 
-        <div class="bg-gray-900 rounded-lg w-80"></div>
+        <div class="w-80 p-2">
+            <div
+                class="participants rounded-xl bg-white shadow-xl shadow-gray-200/50 overflow-hidden">
+                <div class="bg-blue-400/10 rounded-2xl py-5 sticky top-0 z-20">
+                    <Label class="capitalize font-bold ml-4 text-blue-700">participants (5)</Label>
+                </div>
+                <div class="max-h-[200px] overflow-y-auto pb-5 custom-scroll">
+                    <div v-for="participant in participants" :key="participant.name"
+                         class="flex items-center gap-2 mt-1 p-2">
+                        <img :alt="participant.name"
+                             :class="participant.isHost && 'outline-3 outline-[#FF4D3C]'"
+                             :src="participant.image"
+                             class="size-9 ml-3 rounded-full border-2 border-white">
+                        <Label class="text-slate-900 text-sm">
+                            {{ participant.name }}
+                            {{ participant.isHost && '(Host)' }}
+                        </Label>
+                    </div>
+                </div>
+            </div>
+            <div class="chat"></div>
+        </div>
     </div>
 </template>
+
+<style scoped>
+.custom-scroll::-webkit-scrollbar {
+    width: 6px;
+}
+
+.custom-scroll::-webkit-scrollbar-button {
+    display: none;
+}
+
+.custom-scroll::-webkit-scrollbar-track {
+    background-color: rgba(255, 77, 60, 0.18);
+}
+
+.custom-scroll::-webkit-scrollbar-thumb {
+    background-color: #FF4D3C;
+    border-radius: 10px;
+}
+
+.custom-scroll::-webkit-scrollbar-thumb:hover {
+    background-color: #e04435;
+}
+</style>
